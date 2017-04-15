84bce0e4800c4b3f22e3118bb2b577f3905c040d
Antonella94duarte <liz94antonella@gmail.com>
2017-04-15 19:53:58 -0400

    Crear Editar servicio, aun no termine

--- app/src/main/java/com/app/mbaappo/mbaappo/CrearEditarServicio.java
+++ app/src/main/java/com/app/mbaappo/mbaappo/CrearEditarServicio.java
@@ -1,13 +1,68 @@
 package com.app.mbaappo.mbaappo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import java.util.ArrayList;
 
-public class CrearEditarServicio extends AppCompatActivity {
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class CrearEditarServicio extends Activity implements  AdapterView.OnItemSelectedListener{
+    public Spinner spTarifa, spCateg;
+    public ArrayAdapter<String> aaTarifa;
+    public ArrayAdapter<String> aaCateg;
+
+    public Categorias cat = new Categorias();
+    String[] opTarifa = new String[]{"Por hora", "Precio Fijo", "A convenir"};
+    ArrayList<String> opCateg = new ArrayList<String>();
+
+    private void CargarCategoria(){
+        opCateg.add(cat.getNombre());
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_editar_servicio);
+
+
+        spTarifa = (Spinner) findViewById(R.id.spTarifa);
+        spTarifa.setOnItemSelectedListener(this);
+        aaTarifa = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opTarifa);
+
+        spTarifa.setAdapter(aaTarifa);
+
+        spCateg = (Spinner) findViewById(R.id.spCateg);
+        spCateg.setOnItemSelectedListener(this);
+        aaCateg = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opCateg);
+
+        spTarifa.setAdapter(aaTarifa);
+
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
 }
