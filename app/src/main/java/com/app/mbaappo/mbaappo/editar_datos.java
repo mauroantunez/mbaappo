dbeae4d842a40ede4353c4a87ad663d70e3ca2e1
mauroantunez <antunez.mauro@gmail.com>
2017-05-20 19:10:58 -0400

    se agrego editar datos

---
+++ app/src/main/java/com/app/mbaappo/mbaappo/editar_datos.java
@@ -0,0 +1,13 @@
+package com.app.mbaappo.mbaappo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editar_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_datos);
+    }
+}
